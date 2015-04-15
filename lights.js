0fdf2b0ebeb99d7bfa60971134a732f7e32bf872
David Young <ohnoitsyou7043@gmail.com>
2015-04-15 00:17:36 -0400

    Fixing the structure

--- lights.js
+++ lights.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 var Lights = function() {
   this.version = '0.1.0',
-  this.load: function() {
+  this.load = function() {
     var requires = {'spark': '0.1.0'};
     return requires;
   },
-  this.initilize: function() {
+  this.initilize = function() {
     // here is where the module should login to spark
     // What would make this better would be a spark module
     //  so that any other plugins that utilize the spark api
