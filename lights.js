e148cc3eafb375ce063ee98f9f680d90d4092f94
David Young <ohnoitsyou7043@gmail.com>
2015-04-23 01:01:06 -0400

    Adding routes, adding debug

--- lights.js
+++ lights.js
@@ -1,19 +1,32 @@
 var express = require('express');
-var router = express.Router();
+var debug = require('debug')('lights');
 
 var Lights = function() {
   this.version = '0.1.0',
-  this.load = function() {
-    var requires = {'spark': '0.1.0'};
-    return requires;
+  this.requires = [{'spark': '0.1.0'}],
+  this.router = express.Router(),
+  this.spark,
+  this.load = function(options) {
+    debug('[Load] Starting');
+    debug('[Load] Finishing');
   },
   this.initilize = function() {
+    debug('[Initilize] Starting');
     // here is where the module should login to spark
     // What would make this better would be a spark module
     //  so that any other plugins that utilize the spark api
     //  don't have to each login.
     //  Shared credentials
+    debug('[Initilize] Finishing');
     return true;
+  },
+  this.loadRoutes = function() {
+    debug('[LoadRoutes] Starting');
+    this.router.get('/', function(req, res) {
+      res.send('Lights!');
+    });
+    debug('[LoadRoutes] Finishing');
+    return this.router;
   }
 }
 
