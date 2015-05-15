59567a81e1844f45d808634ba9761a6879fd3d78
David Young <ohnoitsyou7043@gmail.com>
2015-05-15 18:58:59 -0400

    Basic device control

--- lights.js
+++ lights.js
@@ -1,11 +1,13 @@
 "use strict";
 var express = require("express");
 var debug = require("debug")("lights");
+var request = require("request");
 
 var Lights = function() {
   this.version = "0.1.0";
   this.requires = [{"spark": "0.1.0"}];
   this.router = express.Router();
+  this.baseURI = "http://automata.ohnoitsyou.net";
   //this.spark;
   this.load = function(options) {
     debug("[Load] Starting");
@@ -23,13 +25,35 @@ var Lights = function() {
   };
   this.loadRoutes = function() {
     debug("[LoadRoutes] Starting");
+    var self = this;
     this.router.get("/", function(req, res) {
+      request("/api/spark/sendCommand/lights/off", function(e, r, b) {
+        if(!e && r.statusCode == 200) {
+          debug("Success");
+        } else {
+          debug("Not Success: %s", e);
+        }
+      });
       res.send("Lights!");
     });
     this.router.get("/on", function(req, res) {
+      request(self.baseURI + "/api/spark/sendCommand/lights/on", function(e, r, b) {
+        if(!e && r.statusCode == 200) {
+          debug("Success");
+        } else {
+          debug("Not Success: %s", e);
+        }
       res.send("Lights on!");
+      });
     });
     this.router.get("/off", function(req, res) {
+      request(self.baseURI + "/api/spark/sendCommand/lights/off", function(e, r, b) {
+        if(!e && r.statusCode == 200) {
+          debug("Success");
+        } else {
+          debug("Not Success: %s", e);
+        }
+      });
       res.send("Lights off!");
     });
     debug("[LoadRoutes] Finishing");
