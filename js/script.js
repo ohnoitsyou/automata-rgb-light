ce62eeda01e7627309f70d1a5cd255bc49bc26f6
David Young <ohnoitsyou7043@gmail.com>
2015-06-16 03:12:08 -0400

    Adding new javascript methods

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ function lightsOff() {
     }
   });
 }
+function toggleSilent() {
+  new Ajax.Request("/api/lights/oniy-lights/ts", {
+    method: "get",
+    onSuccess: function(data) {
+      console.log(data.responseText);
+    }
+  });
+}
+function setMode(mode) {
+  new Ajax.Request("/api/lights/oniy-lights/ro/" + mode, {
+    method: "get",
+    onSuccess: function(data) {
+      console.log(data.responseText);
+    }
+  });
+}
